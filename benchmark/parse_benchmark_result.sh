a60e6464747c37ef27e9312f7623d023c2b25096
changcui <Chang.Cui@amd.com>
2025-07-15 14:24:59 +0800

    update parse benchmark

--- benchmark/parse_benchmark_result.sh
+++ benchmark/parse_benchmark_result.sh
@@ -7,15 +7,25 @@ fi
 
 file="$1"
 
+if ! command -v jq >/dev/null; then
+    echo "Error: jq is not installed." >&2
+    exit 1
+fi
+
+echo -e "request_throughput (req/s)\tinput_throughput (tok/s)\toutput_throughput (tok/s)\ttotal_throughput (tok/s)\tmean_ttft (ms)\tmedian_ttft (ms)\tmean_tpot (ms)\tmedian_tpot (ms)"
+
 jq -r '
   select(.request_throughput != null) |
-  . as $obj |
-  "request_throughput: \($obj.request_throughput)\n" +
-  "input_throughput: \($obj.input_throughput)\n" +
-  "output_throughput: \($obj.output_throughput)\n" +
-  "total_throughput: \($obj.input_throughput + $obj.output_throughput)\n" +
-  "mean_ttft_ms: \($obj.mean_ttft_ms)\n" +
-  "median_ttft_ms: \($obj.median_ttft_ms)\n" +
-  "mean_tpot_ms: \($obj.mean_tpot_ms)\n" +
-  "median_tpot_ms: \($obj.median_tpot_ms)"
-' "$file"
+  [
+    .request_throughput,
+    .input_throughput,
+    .output_throughput,
+    (.input_throughput + .output_throughput),
+    .mean_ttft_ms,
+    .median_ttft_ms,
+    .mean_tpot_ms,
+    .median_tpot_ms
+  ] | @tsv
+' "$file" | awk -F '\t' '{
+  printf "%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n", $1, $2, $3, $4, $5, $6, $7, $8
+}'
